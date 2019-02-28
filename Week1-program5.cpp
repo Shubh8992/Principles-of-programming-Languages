e574130ee7875ebeefa7b09577491ec81dc3c7b6
shrutijain501 <42448037+shrutijain501@users.noreply.github.com>
2019-02-28 20:31:27 +0530

    Add files via upload

---
+++ Week1-program5.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long int x,y;
+	cout<<"Input X and Y:\n";
+	cin>>x>>y;
+	cout<<"(x+y)/(x-y)="<<(x+y)/(x-y)<<"\n";
+	cout<<"(x-y)(x+y)="<<(x-y)*(x+y)<<"\n";
+}
